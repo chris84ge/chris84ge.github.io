8379d538d6e82d791d658e19adf5c3e18c28f94d
strayChris <chsalus@gmail.com>
2018-10-15 17:18:39 +0300

    it won't work

---
+++ attemp1/build/precache-manifest.b4ba878ae64eadd4b32eea64ed59252f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49553c3bb6b723f139ce",
+    "url": "/static/js/main.49553c3b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "49553c3bb6b723f139ce",
+    "url": "/static/css/main.397c6327.chunk.css"
+  },
+  {
+    "revision": "fa6f623ccf54dbbe68c929bbd2d25f04",
+    "url": "/index.html"
+  }
+];
